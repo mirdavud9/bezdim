bdc0c5f5d445e1dc1d976d8c24f3b456240368c9
mirdavud9 <mirdavudvm-bp217@code.edu.az>
2024-10-14 00:34:09 +0400

    _initial

---
+++ yeni nese/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using yeni_nese;
+
+namespace bezdim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            student student = new student("Kitabulla", "Qarayev", "Bm99", 98);
+            student student2 = new student("KERIM", "Agayev", "KL-76", 64);
+            student.GetInfo();
+            student.GetDiplomDegree();
+            student.CheckGraduation();
+            student2.GetInfo();
+            student2.CheckGraduation();
+            student2.GetDiplomDegree();
+        }
+    }
+}
---
+++ yeni nese/student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yeni_nese
+{
+    internal class student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public byte Point;
+        public bool IsGraduated;
+
+        public student(string name, string surname, string group, byte point)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            CheckGraduation();
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Surname: {Surname}, Group: {Group}, Point: {Point}, IsGraduated: {IsGraduated}");
+        }
+        public void CheckGraduation()
+        {
+            IsGraduated = Point >= 65;
+
+            if (IsGraduated)
+            {
+                Console.WriteLine("Finished");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+
+
+
+        public void GetDiplomDegree()
+        {
+            if (Point < 65)
+            {
+                Console.WriteLine("None");
+            }
+            else if (Point <= 80)
+            {
+                Console.WriteLine("Simple");
+            }
+            else if (Point <= 90)
+            {
+                Console.WriteLine("Honor");
+            }
+            else 
+            {
+                Console.WriteLine("High honor");
+            }
+        }
+    }
+}
